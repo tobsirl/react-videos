352509af25a6b6e7a1153e2f1984e880c23cda5f
Paul Tobin <tobster111@gmail.com>
2019-04-08 14:19:27 +0100

    Added the SearchBar component + bootstrap cdn

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+import SearchBar from './SearchBar';
+
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>App Component</h1>
+        <SearchBar />
       </div>
     );
   }
