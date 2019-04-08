1d6af70177b4dab48038556e6afd320bb85e5558
Paul Tobin <tobster111@gmail.com>
2019-04-08 16:18:36 +0100

    Added state to the SearchBar component + onChangeInput function

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import SearchBar from './SearchBar';
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="ui container">
         <SearchBar />
       </div>
     );
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  state = {
+    term: ''
+  };
+
+  onChangeInput = event => {
+    this.setState({ term: event.target.value });
+    console.log(this.state.term);
+  };
+
   render() {
     return (
-      <div>
-        <h1>Search Bar</h1>
+      <div className="search-bar ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onChangeInput}
+            />
+          </div>
+        </form>
       </div>
     );
   }
