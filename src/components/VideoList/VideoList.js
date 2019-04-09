16a3a73c12b568f7a53b64eba1617f122a7457c7
Paul Tobin <tobster111@gmail.com>
2019-04-09 15:42:37 +0100

    Added VideoItem component

--- src/components/VideoList/VideoList.js
+++ src/components/VideoList/VideoList.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
-const VideoList = props => {
-  console.log(props.videos);
-  return <div>{props.videos.length}</div>;
+import VideoItem from '../VideoItem/VideoItem';
+
+const VideoList = ({ videos }) => {
+  console.log(videos);
+  const renderedList = videos.map(video => {
+    return <VideoItem video={renderedList} />;
+  });
+  return <div>{renderedList}</div>;
 };
 
 export default VideoList;
