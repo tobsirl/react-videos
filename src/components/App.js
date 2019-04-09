3c268a2ac4290007e14a1aba03af80cc0318b522
Paul Tobin <tobster111@gmail.com>
2019-04-09 15:05:48 +0100

    Added state to App.js, used setState to store the videos objects

--- src/components/App.js
+++ src/components/App.js
@@ -5,20 +5,25 @@ import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 
 class App extends Component {
-  onTermSubmit = term => {
-    youtube
-      .get('/search', {
-        params: {
-          q: term
-        }
-      })
-      .then(res => console.log(res.data.items));
+  state = {
+    videos: []
+  };
+
+  onTermSubmit = async term => {
+    const response = await youtube.get('/search', {
+      params: {
+        q: term
+      }
+    });
+    console.log(response.data.items);
+    this.setState({ videos: response.data.items });
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        I have {this.state.videos.length}
       </div>
     );
   }
