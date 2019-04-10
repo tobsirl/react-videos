128bdca21124b0a7b7a026210876ed17ba856be7
Paul Tobin <tobster111@gmail.com>
2019-04-10 15:48:15 +0100

    onTermSubmit now updates selectedVideo to provide a default video

--- src/components/App.js
+++ src/components/App.js
@@ -19,7 +19,10 @@ class App extends Component {
       }
     });
 
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    });
   };
 
   onVideoSelect = video => {
